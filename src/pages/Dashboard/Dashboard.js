e23e340e1c6d74b4b48e867f0c2ae1e1c2ecf6ea
De Roover Justin <justinderoover@gmail.com>
2024-03-10 23:38:45 +0100

    add btn add note

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { NoteList } from "../../components";
+import { NoteList, NoteForm } from "../../components";
 
 function Dashboard() {
   return (
     <div className="dashboard">
+      <NoteForm />
       <h1>My Notes</h1>
       <NoteList />
     </div>
