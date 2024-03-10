0fe44ceb704034562cc5a0459d4a664d77aea081
De Roover Justin <justinderoover@gmail.com>
2024-03-10 23:22:59 +0100

    add .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJM-8u5LJ_QE1IG1VZzNkNTAyoXHPW5Sc",
-  authDomain: "fir-notes-c9335.firebaseapp.com",
-  projectId: "fir-notes-c9335",
-  storageBucket: "fir-notes-c9335.appspot.com",
-  messagingSenderId: "58549928768",
-  appId: "1:58549928768:web:3fd11a5a7977cb6b2570a5",
-  measurementId: "G-E7ZWCPGZGB",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialiser Firebase
